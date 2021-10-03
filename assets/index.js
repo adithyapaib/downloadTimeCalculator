c5c4b05aea085718b004f766604e6be3013e15f5
Raj <Raj.madhu0406@gmail.com>
2021-10-03 22:00:47 +0530

    updated darkmode

--- assets/index.js
+++ assets/index.js
@@ -1,44 +1,49 @@
-document.getElementById("submit").addEventListener("click",best);
-function best()
-{
+document.getElementById("submit").addEventListener("click", best);
+function best() {
     let speed = document.getElementById("speed").value;
     let size = document.getElementById("size").value;
-    if(speed=="" || size=="")
-    {
+    if (speed == "" || size == "") {
         alert("Please fill all the fields");
     }
-    else
-    {
+    else {
         let downloadSpeed = speed;
-        let oneGB = parseFloat(1024/downloadSpeed);
+        let oneGB = parseFloat(1024 / downloadSpeed);
         let oneGBhours = Math.floor(oneGB / 60 / 60);
         let oneGBminutes = Math.floor(oneGB / 60) - (oneGBhours * 60);
-        let oneGBseconds = Math.round(oneGB % 60,3);
+        let oneGBseconds = Math.round(oneGB % 60, 3);
         let oneGBTime;
         let minutes;
         minutes = 'mins';
-        if(parseInt(oneGBminutes)==1)
+        if (parseInt(oneGBminutes) == 1)
             minutes = `min`
-        oneGBTime =  `${oneGBhours}hrs, ${oneGBminutes}${minutes}, ${oneGBseconds}s `
-        if(oneGBhours == 0)
-            oneGBTime =  `${oneGBminutes}${minutes}, ${oneGBseconds}s `
-        let file =  (parseInt(size)*1024)/downloadSpeed;
-        let fileHours =  Math.floor(file / 60 / 60);
+        oneGBTime = `${oneGBhours}hrs, ${oneGBminutes}${minutes}, ${oneGBseconds}s `
+        if (oneGBhours == 0)
+            oneGBTime = `${oneGBminutes}${minutes}, ${oneGBseconds}s `
+        let file = (parseInt(size) * 1024) / downloadSpeed;
+        let fileHours = Math.floor(file / 60 / 60);
         let fileMinutes = Math.floor(file / 60) - (fileHours * 60);
-        let fileSeconds = Math.round(file % 60,3);
+        let fileSeconds = Math.round(file % 60, 3);
         let fileTime;
         minutes = 'mins';
-        if(parseInt(fileMinutes)==1)
+        if (parseInt(fileMinutes) == 1)
             minutes = `min`
-        fileTime =  `${fileHours}hrs, ${fileMinutes}${minutes}, ${fileSeconds}s `;
+        fileTime = `${fileHours}hrs, ${fileMinutes}${minutes}, ${fileSeconds}s `;
         document.getElementById('output').style.display = 'block'
         document.getElementById("download").innerHTML = `Download speed: ${downloadSpeed}MBps`;
         document.getElementById("sent").innerHTML = `Approximate time required to download`;
         document.getElementById("onespeed").innerHTML = `1 GB file: ${oneGBTime}`;
         document.getElementById("filespeed").innerHTML = `${size}GB file: ${fileTime}`;
         document.getElementById("reload").innerHTML = `<br><br><a href="index.html" class="w-full text-center bg-black text-white p-4 rounded-md">CALCULATE NEW</a>`;
-        document.getElementById("output").style.background='#07C962'
+        document.getElementById("output").style.background = '#07C962'
         console.log(oneGBTime);
         console.log(fileTime)
     }
+}
+
+
+function darkmode() {
+
+    var body = document.body;
+    body.classList.toggle("dark");
+    document.getElementById("submit").classList.toggle("submit-dark");
 }
